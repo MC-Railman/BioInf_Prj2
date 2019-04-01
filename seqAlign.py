0c3dd6146742d56c0182bb02ac2d50d414aa0d92
Tristan Hess <tlh173@zips.uakron.edu>
2019-04-01 13:48:25 -0400

    create project file and add To-do list

---
+++ seqAlign.py
@@ -0,0 +1,18 @@
+# A sequence aligning program for Dr. Duan's Bioinformatics class
+# Written by Tristan Hess, Scott Owens, and Ryan Weston
+
+print("Hello world!")
+
+# We are doing sequence 4 of Influenza A and are presenting as group #2 on Wednesday 4/3
+
+#TODO use genbank to select two localities of Influenza A sequence 4 to align
+                # EX: Ohio and Shanghai alignment
+#TODO implement pairwise alignment between the two sequences
+
+#TODO count # of mutations: indels, synonymous mutations, nonsynonymous mutations, and other stuff
+
+#TODO display data has a table
+
+#TODO each of us needs to write our own individual report using the data we find
+
+#TODO make a presentation for wednesday
