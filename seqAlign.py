dddf5d4a04e553e1c2eb7b3e0f22c9f3a65256d6
Tristan Hess <tlh173@zips.uakron.edu>
2019-04-01 21:39:39 -0400

    create starting variables for the sequence alignment

--- seqAlign.py
+++ seqAlign.py
@@ -1,12 +1,73 @@
 # A sequence aligning program for Dr. Duan's Bioinformatics class
 # Written by Tristan Hess, Scott Owens, and Ryan Weston
 
-print("Hello world!")
+# Sequence 4 from California- https://www.ncbi.nlm.nih.gov/nuccore/NC_026433.1
+gene1 = "atgaaggcaatactagtagttctgctatatacatttgcaaccgcaaatgcagacacatta" \
+        "tgtataggttatcatgcgaacaattcaacagacactgtagacacagtactagaaaagaat" \
+        "gtaacagtaacacactctgttaaccttctagaagacaagcataacgggaaactatgcaaa" \
+        "ctaagaggggtagccccattgcatttgggtaaatgtaacattgctggctggatcctggga" \
+        "aatccagagtgtgaatcactctccacagcaagctcatggtcctacattgtggaaacacct" \
+        "agttcagacaatggaacgtgttacccaggagatttcatcgattatgaggagctaagagag" \
+        "caattgagctcagtgtcatcatttgaaaggtttgagatattccccaagacaagttcatgg" \
+        "cccaatcatgactcgaacaaaggtgtaacggcagcatgtcctcatgctggagcaaaaagc" \
+        "ttctacaaaaatttaatatggctagttaaaaaaggaaattcatacccaaagctcagcaaa" \
+        "tcctacattaatgataaagggaaagaagtcctcgtgctatggggcattcaccatccatct" \
+        "actagtgctgaccaacaaagtctctatcagaatgcagatgcatatgtttttgtggggtca" \
+        "tcaagatacagcaagaagttcaagccggaaatagcaataagacccaaagtgagggrtcra" \
+        "gaagggagaatgaactattactggacactagtagagccgggagacaaaataacattcgaa" \
+        "gcaactggaaatctagtggtaccgagatatgcattcgcaatggaaagaaatgctggatct" \
+        "ggtattatcatttcagatacaccagtccacgattgcaatacaacttgtcaaacacccaag" \
+        "ggtgctataaacaccagcctcccatttcagaatatacatccgatcacaattggaaaatgt" \
+        "ccaaaatatgtaaaaagcacaaaattgagactggccacaggattgaggaatatcccgtct" \
+        "attcaatctagaggcctatttggggccattgccggtttcattgaaggggggtggacaggg" \
+        "atggtagatggatggtacggttatcaccatcaaaatgagcaggggtcaggatatgcagcc" \
+        "gacctgaagagcacacagaatgccattgacgagattactaacaaagtaaattctgttatt" \
+        "gaaaagatgaatacacagttcacagcagtaggtaaagagttcaaccacctggaaaaaaga" \
+        "atagagaatttaaataaaaaagttgatgatggtttcctggacatttggacttacaatgcc" \
+        "gaactgttggttctattggaaaatgaaagaactttggactaccacgattcaaatgtgaag" \
+        "aacttatatgaaaaggtaagaagccagctaaaaaacaatgccaaggaaattggaaacggc" \
+        "tgctttgaattttaccacaaatgcgataacacgtgcatggaaagtgtcaaaaatgggact" \
+        "tatgactacccaaaatactcagaggaagcaaaattaaacagagaagaaatagatggggta" \
+        "aagctggaatcaacaaggatttaccagattttggcgatctattcaactgtcgccagttca" \
+        "ttggtactggtagtctccctgggggcaatcagtttctggatgtgctctaatgggtctcta" \
+        "cagtgtagaatatgtatttaa"
 
-# We are doing sequence 4 of Influenza A and are presenting as group #2 on Wednesday 4/3
+# Sequence 4 from New York- https://www.ncbi.nlm.nih.gov/nuccore/NC_007366.1
+gene2 = "agcaaaagcaggggataattctattaaccatgaagactatcattgctttgagctacattc" \
+        "tatgtctggttttcgctcaaaaacttcccggaaatgacaacagcacggcaacgctgtgcc" \
+        "ttgggcaccatgcagtaccaaacggaacgatagtgaaaacaatcacgaatgaccaaattg" \
+        "aagtcactaatgctactgaactggttcagagttcctcaacaggtggaatatgcgacagtc" \
+        "ctcatcagatccttgatggagaaaactgcacactaatagatgctctattgggagaccctc" \
+        "agtgtgatggcttccaaaataagaaatgggacctttttgttgaacgcagcaaagcctaca" \
+        "gcaactgttacccttatgatgtgccggattatgcctcccttaggtcactagttgcctcat" \
+        "ccggcacactggagtttaacaatgaaagcttcaattggactggagtcactcaaaatggaa" \
+        "caagctctgcttgcaaaaggagatctaataacagtttctttagtagattgaattggttga" \
+        "cccacttaaaattcaaatacccagcattgaacgtgactatgccaaacaatgaaaaatttg" \
+        "acaaactgtacatttggggggttcaccacccgggtacggacaatgaccaaatcagcctat" \
+        "atgctcaagcatcaggaagaatcacagtctctaccaaaagaagccaacaaaccgtaatcc" \
+        "cgagtatcggatctagacccaggataagggatgtccccagcagaataagcatctattgga" \
+        "caatagtaaaaccgggagacatacttttgattaacagcacagggaatctaattgctcctc" \
+        "ggggttacttcaaaatacgaagtgggaaaagctcaataatgagatcagatgcacccattg" \
+        "gcaaatgcaattctgaatgcatcactccaaatggaagcattcccaatgacaaaccatttc" \
+        "aaaatgtaaacaggatcacatatggggcctgtcccagatatgttaagcaaaacactctga" \
+        "aattggcaacagggatgcgaaatgtaccagagaaacaaactagaggcatatttggcgcaa" \
+        "tcgcgggtttcatagaaaatggttgggagggaatggtagacggttggtacggtttcaggc" \
+        "atcaaaattctgagggaacaggacaagcagcagatctcaaaagcactcaagcagcaatca" \
+        "accaaatcaatgggaagctgaataggttgatcgggaaaacaaacgagaaattccatcaga" \
+        "ttgaaaaagaattctcagaagtagaagggagaattcaggacctcgagaaatatgttgagg" \
+        "acactaaaatagatctctggtcatacaacgcggagcttcttgtggccctggagaaccaac" \
+        "atacaattgatctaactgactcagaaatgaacaaactgtttgaaagaacaaagaagcaac" \
+        "tgagggaaaatgctgaggatatgggcaatggttgtttcaaaatataccacaaatgtgaca" \
+        "atgcctgcatagggtcaatcagaaatggaacttatgaccatgatgtatacagagatgaag" \
+        "cattaaacaaccggttccagatcaaaggtgttgagttgaagtcaggatacaaagattgga" \
+        "tcctatggatttcctttgccatatcatgttttttgctttgtgttgctttgttggggttca" \
+        "tcatgtgggcctgccaaaaaggcaacattaggtgcaacatttgcatttgagtgcattaat" \
+        "taaaaacacccttgtttctact"
 
-#TODO use genbank to select two localities of Influenza A sequence 4 to align
-                # EX: Ohio and Shanghai alignment
+print(gene1)
+print(gene2)
+
+
 #TODO implement pairwise alignment between the two sequences
 
 #TODO count # of mutations: indels, synonymous mutations, nonsynonymous mutations, and other stuff
