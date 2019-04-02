e45ad859cbf6142051f6fa11fd3ccdc38863a403
Tristan Hess <tlh173@zips.uakron.edu>
2019-04-01 22:39:59 -0400

    create framework for global alignment matrix and update TODO comments

--- seqAlign.py
+++ seqAlign.py
@@ -1,6 +1,12 @@
 # A sequence aligning program for Dr. Duan's Bioinformatics class
 # Written by Tristan Hess, Scott Owens, and Ryan Weston
 
+import numpy
+
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# OH YEAH I SHOULD PROBABLY MENTION IM USING PYTHON 3
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
 # Sequence 4 from California- https://www.ncbi.nlm.nih.gov/nuccore/NC_026433.1
 gene1 = "atgaaggcaatactagtagttctgctatatacatttgcaaccgcaaatgcagacacatta" \
         "tgtataggttatcatgcgaacaattcaacagacactgtagacacagtactagaaaagaat" \
@@ -65,10 +71,34 @@ gene2 = "agcaaaagcaggggataattctattaaccatgaagactatcattgctttgagctacattc" \
         "taaaaacacccttgtttctact"
 
 print(gene1)
+print(len(gene1))
 print(gene2)
+print(len(gene2))
 
 
-#TODO implement pairwise alignment between the two sequences
+#TODO implement global/semi-global/local alignment between the two sequences
+# Global alignment might be better with such a large gene sequence. It starts at the end instead of the highest
+# score in the matrix. We wouldn't need to make pointers or anything, just compare the values around the current one
+# to select a match and go. Only limitation i see if the processing power and memory in our computers to do a
+# 1701 x 1762 matrix of numbers.. lol
+# Going to start with a global alignment and see if I regret my decisions later.. - Tristan
+
+# Variables for scoring
+gap = -2
+mismatch = -1
+match = 1
+
+# Global alignment matrix. Had to use numpy for this.
+# Creates an EMPTY 1702 x 1763 matrix for scoring (one extra than the length for the first row comparing the sequences to nothing
+# Use this like matrix[0][0] for the first entry, matrix[0][1] for the one below it, and matrix[1][0] for the one beside
+matrix = numpy.empty([1702,1763])
+alignment = ""
+
+#TODO fill the alignment using the scoring values. We can use a nester for-loop for this, and fill it like we did with the ones in class
+# If this is a terrible idea and there's an easier way to do this, feel free to dump my stuff I have here xD - Tristan
+
+
+#TODO compare our sequence alignment to BLAST on the genbank website
 
 #TODO count # of mutations: indels, synonymous mutations, nonsynonymous mutations, and other stuff
 
